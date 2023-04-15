39a008c3a8f7b8b3737e94a1d59f540b0fb325f4
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-04-15 15:52:13 +0300

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+    static String ArabicToRoman(int aNumber){
+
+        if(aNumber < 1 || aNumber > 3999){
+            return "-1";
+        }
+
+        int[] aArray = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] rArray = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String rNumber = "";
+
+        for(int i=0; i<aArray.length; i++){
+            while(aNumber >= aArray[i]){
+                rNumber += rArray[i];
+                aNumber -= aArray[i];
+            }
+        }
+
+        return rNumber;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[] allowedSigns = {'+', '-', '/', '*'};
     }
 }
