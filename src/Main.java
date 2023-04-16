8e47a89ddcdd97779fbeff105a802a85e1247365
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-04-16 12:59:08 +0300

    type problem

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-import java.util.Iterator;
-
 public class Main {
 
-    static String ArabicToRoman(int aNumber){
-        if(aNumber < 1 || aNumber > 3999){
+    static String ArabicToRoman(int aNumber) {
+        if (aNumber < 1 || aNumber > 3999) {
             return "-1";
         }
 
-        int[] aArray = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] rArray = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] aArray = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] rArray = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         String rNumber = "";
 
-        for(int i=0; i<aArray.length; i++){
-            while(aNumber >= aArray[i]){
+        for (int i = 0; i < aArray.length; i++) {
+            while (aNumber >= aArray[i]) {
                 rNumber += rArray[i];
                 aNumber -= aArray[i];
             }
@@ -21,9 +19,10 @@ public class Main {
         return rNumber;
     }
 
-    public static String calc(String input){
-        String[] allowedSigns = {"+", "-", "/", "*"};
+    public static String calc(String input) {
+        return "";
     }
+
     public static void main(String[] args) {
     }
 }
