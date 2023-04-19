f70a857bc0dcfdd7ed80bebaca53ea0d5460a065
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-04-19 19:04:05 +0300

    romanToArabic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Main {
+    enum Roman {
+        I(1), V(5), X(10), L(50), C(100), D(500), M(1000);
+        private final int value;
+        private Roman(int value) {
+            this.value = value;
+        }
+        public int toInt() {
+            return value;
+        }
+    }
+    static int romanToArabic(String input) {
+        String romanNumeral = input.toUpperCase();
+        int result = 0;
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+
+        while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+            RomanNumeral symbol = romanNumerals.get(i);
+            if (romanNumeral.startsWith(symbol.name())) {
+                result += symbol.getValue();
+                romanNumeral = romanNumeral.substring(symbol.name().length());
+            } else {
+                i++;
+            }
+        }
+
+        if (romanNumeral.length() > 0) {
+            throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+        }
+
+        return result;
+    }
+
+    enum RomanNumeral {
+        I(1), IV(4), V(5), IX(9), X(10),
+        XL(40), L(50), XC(90), C(100),
+        CD(400), D(500), CM(900), M(1000);
+
+        private int value;
+
+        RomanNumeral(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static List<RomanNumeral> getReverseSortedValues() {
+            return Arrays.stream(values())
+                    .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
+                    .collect(Collectors.toList());
+        }
+    }
     static String ArabicToRoman(int aNumber) {
         if (aNumber < 1 || aNumber > 3999) {
             return "-1";
@@ -15,14 +76,15 @@ public class Main {
                 aNumber -= aArray[i];
             }
         }
-
         return rNumber;
     }
 
     public static String calc(String input) {
+
         return "";
     }
 
     public static void main(String[] args) {
+        System.out.println(romanToArabic("X"));
     }
 }
